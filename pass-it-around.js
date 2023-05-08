7156c4f94dd975b4a564e5518ae392a83840a9fb
Papa Ndoye <pape2006ndoye@hotmail.com>
2023-05-08 08:56:51 -0400

    commit

--- pass-it-around.js
+++ pass-it-around.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express")
+const app = express()
+const port = 3000
 
 app.get("/", (req, res) => {
   res.send(`<h3>99 Bottles of beer on the wall</h3>
@@ -11,12 +11,12 @@ app.get("/:numOfBottles", (req, res) => {
   let bottleNumbers = req.params.numOfBottles;
   if (bottleNumbers > 0) {
     res.send(`<h3>${bottleNumbers} Bottles of beer on the wall</h3>
-        <a href ="/${bottleNumbers - 1}">Take one down, pass it around</a>`);
+        <a href ="/${bottleNumbers - 1}">Take one down, pass it around</a>`)
   } else {
-    res.send(`No more beers! <a href ="/">Please start over</a>`);
+    res.send(`No more beers! <a href ="/">Please start over</a>`)
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`)
 });
